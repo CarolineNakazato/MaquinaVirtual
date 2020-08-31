1f9e4a06e56a535e89b045a870431b9bc6bfe41c
CarolineNakazato <carolinenakazato@yahoo.com.br>
2020-08-30 21:00:02 -0300

    AND, INV, NEG, DIVI, MULT

--- Programa.java
+++ Programa.java
@@ -41,6 +41,20 @@ public class Programa {
         M.set(s-1, value);
         this.s--;
     }
+    //MULT (Multiplicar):
+    //M[s-1]:=M[s-1] * M[s]; s:=s - 1
+    public void mult() {
+        int value = Integer.parseInt(M.get(s-1).toString())* Integer.parseInt(M.get(s).toString());
+        M.set(s-1, value);
+        this.s--;
+    }
+    //DIVI (Dividir):
+    //M[s-1]:=M[s-1] div M[s]; s:=s - 1 
+    public void divi() {
+        int value = Integer.parseInt(M.get(s-1).toString())/ Integer.parseInt(M.get(s).toString());
+        M.set(s-1, value);
+        this.s--;
+    }
     public void start(){
         this.s = -1;
     }
@@ -56,7 +70,29 @@ public class Programa {
         this.s++;
          M.add(s, M.get(n));
     }
-    
+    //INV (Inverter sinal):
+    //M[s]:= -M[s] 
+    public void inv(){
+        int value = Integer.parseInt(M.get(s).toString()) * (-1);
+        M.set(s, value);
+    }
+    //AND (Conjunção):
+    //se M [s-1] = 1 e M[s] = 1 então M[s-1]:=1 senão M[s-1]:=0; s:=s - 1 
+     public void and(){
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0;
+        if ((value2 == 1) && (value1 == 1)) 
+            result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+    //NEG (Negação):
+    //M[s]:=1 - M[s] 
+    public void neg(){
+        int value = 1 - Integer.parseInt(M.get(s).toString());
+        M.set(s, value);
+    }
     public Stack debug(int numeroInstrucao){
         String[] linha = P.getValue(numeroInstrucao).split(" ");
         //int tamanhoLinha = linha.length;
@@ -77,7 +113,27 @@ public class Programa {
                         if(linha[0].equals("SUB")){
                             sub();
                         }else{
+                            if(linha[0].equals("MULT")){
+                                mult();
+                            }else{
+                                if(linha[0].equals("DIVI")){
+                                    divi();
+                                }else{
+                                    if(linha[0].equals("NEG")){
+                                        neg();
+                                    }else{
+                                        if(linha[0].equals("INV")){
+                                            inv();
+                                        }else{
+                                            if(linha[0].equals("AND")){
+                                                and();
+                                            }else{
                          
+                                            }
+                                        }
+                                    }
+                                }
+                            }
                         }  
                     }
                 }
