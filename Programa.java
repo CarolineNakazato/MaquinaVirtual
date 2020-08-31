5120bb92208fba4fb36f6052de6bb9b13fe50436
Nícolas Kupka <nic.kupka@hotmail.com>
2020-08-30 21:48:26 -0300

    Feitos: OR, CME, CMA, CEQ, CDIF, CMEQ, CMAQ, HLT

--- Programa.java
+++ Programa.java
@@ -10,7 +10,7 @@ import java.util.Stack;
 
 /**
  *
- * @author Dell
+ * @author Caroline e Nícolas
  */
 public class Programa {
     Stack M;
@@ -20,13 +20,13 @@ public class Programa {
     //i = posicao; s = top
  
     public Programa(String nomeArq) throws IOException{
-       
         P = new Codigo();
         arq = new Arquivo(nomeArq);
         arq.inserirArryList(P);
         M = new Stack();
         i = 0;
     }
+	
     //ADD (Somar):
     //M[s-1]:=M[s-1] + M[s]; s:=s - 1
     public void add(){
@@ -34,13 +34,15 @@ public class Programa {
         M.set(s-1, value);
         this.s--;
     }
+
     //SUB (Subtrair):
     //M[s-1]:=M[s-1] - M[s]; s:=s - 1
-    public void sub(){
+    public void sub() {
         int value = Integer.parseInt(M.get(s-1).toString())- Integer.parseInt(M.get(s).toString());
         M.set(s-1, value);
         this.s--;
     }
+	
     //MULT (Multiplicar):
     //M[s-1]:=M[s-1] * M[s]; s:=s - 1
     public void mult() {
@@ -48,16 +50,127 @@ public class Programa {
         M.set(s-1, value);
         this.s--;
     }
+
     //DIVI (Dividir):
-    //M[s-1]:=M[s-1] div M[s]; s:=s - 1 
+    //M[s-1]:=M[s-1] div M[s]; s:=s - 1
     public void divi() {
         int value = Integer.parseInt(M.get(s-1).toString())/ Integer.parseInt(M.get(s).toString());
         M.set(s-1, value);
         this.s--;
     }
+
+    //INV (Inverter sinal):
+    //M[s]:= -M[s] 
+    public void inv() {
+        int value = Integer.parseInt(M.get(s).toString()) * (-1);
+        M.set(s, value);
+    }
+    
+    //AND (Conjunção):
+    //se M [s-1] = 1 e M[s] = 1 então M[s-1]:=1 senão M[s-1]:=0; s:=s - 1 
+    public void and() {
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0;
+        if (value2 == 1 && value1 == 1) result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+    
+    //OR (Disjunção)
+    //se M[s-1] = 1  ou M[s] = 1  então M[s-1]:=1  senão M[s-1]:=0; s:=s - 1 
+    public void or() {
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0;
+        if (value2 == 1 || value1 == 1) result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+	
+    //NEG (Negação):
+    //M[s]:=1 - M[s] 
+    public void neg(){
+        int value = 1 - Integer.parseInt(M.get(s).toString());
+        M.set(s, value);
+    }
+    
+    //CME (Comparar menor)
+    //se M[s-1] < M[s]  então M[s-1]:=1  senão M[s-1]:=0; s:=s - 1 
+    public void cme() {
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0; 
+        if (value2 < value1) result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+    
+    //CMA (Comparar maior)
+    //se M[s-1] > M[s]  então M[s-1]:=1  senão M[s-1]:=0; s:=s - 1
+    public void cma() {
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0;
+        if (value2 > value1) result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+    
+    //CEQ (Comparar igual)
+    //se M[s-1] = M[s]  então M[s-1]:=1  senão M[s-1]:=0; s:=s - 1 
+    public void ceq() {
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0;
+        if (value2 == value1) result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+    
+    //CDIF (Comparar desigual)
+    //se M[s-1] ≠ M[s]  então M[s-1]:=1  senão M[s-1]:=0; s:=s - 1 
+    public void cdif() {
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0;
+        if (value2 != value1) result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+     
+    //CMEQ (Comparar menor ou igual)
+    //se M[s-1] ≤ M[s]  então M[s-1]:=1  senão M[s-1]:=0; s:=s - 1 
+    public void cmeq() {
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0;
+        if (value2 <= value1) result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+    
+    //CMAQ (Comparar maior ou igual)
+    //se M[s-1] ≥ M[s]  então M[s-1]:=1  senão M[s-1]:=0; s:=s - 1
+    public void cmaq() {
+        int value1 = Integer.parseInt(M.get(s).toString());
+        int value2 = Integer.parseInt(M.get(s-1).toString());
+        int result = 0;
+        if (value2 >= value1) result = 1;
+        M.set(s-1, result);
+        this.s--;
+    }
+    
+    //START (Iniciar Programa Principal)
     public void start(){
         this.s = -1;
     }
+    
+    //HLT (Parar)
+    public void hlt() {
+        M.removeAllElements();
+    }
+    
     //LDC k (Carregar constante):
     //S:=s + 1 ; M [s]: = k 
     public void ldc(int k){
@@ -70,30 +183,8 @@ public class Programa {
         this.s++;
          M.add(s, M.get(n));
     }
-    //INV (Inverter sinal):
-    //M[s]:= -M[s] 
-    public void inv(){
-        int value = Integer.parseInt(M.get(s).toString()) * (-1);
-        M.set(s, value);
-    }
-    //AND (Conjunção):
-    //se M [s-1] = 1 e M[s] = 1 então M[s-1]:=1 senão M[s-1]:=0; s:=s - 1 
-     public void and(){
-        int value1 = Integer.parseInt(M.get(s).toString());
-        int value2 = Integer.parseInt(M.get(s-1).toString());
-        int result = 0;
-        if ((value2 == 1) && (value1 == 1)) 
-            result = 1;
-        M.set(s-1, result);
-        this.s--;
-    }
-    //NEG (Negação):
-    //M[s]:=1 - M[s] 
-    public void neg(){
-        int value = 1 - Integer.parseInt(M.get(s).toString());
-        M.set(s, value);
-    }
-    public Stack debug(int numeroInstrucao){
+    
+     public Stack debug(int numeroInstrucao){
         String[] linha = P.getValue(numeroInstrucao).split(" ");
         //int tamanhoLinha = linha.length;
         System.out.println(linha[0]);
@@ -128,7 +219,38 @@ public class Programa {
                                             if(linha[0].equals("AND")){
                                                 and();
                                             }else{
-                         
+                                                if(linha[0].equals("OR")){
+                                                    or();
+                                                }else{
+                                                    if(linha[0].equals("CME")){
+                                                        cme();
+                                                    }else{
+                                                        if(linha[0].equals("CMA")){
+                                                            cma();
+                                                        }else{
+                                                            if(linha[0].equals("CEQ")){
+                                                                ceq();
+                                                            }else{
+                                                                if(linha[0].equals("CDIF")){
+                                                                    cdif();
+                                                                }else{
+                                                                    if(linha[0].equals("CMEQ")){
+                                                                        cmeq();
+                                                                    }else{
+                                                                        if(linha[0].equals("CMAQ")){
+                                                                            cmaq();
+                                                                        }else{
+                                                                            if(linha[0].equals("HLT")){
+                                                                                hlt();
+                                                                            }else{
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
                                             }
                                         }
                                     }
